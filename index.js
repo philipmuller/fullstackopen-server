6d1d1433a1d3f3b5dd3aa67469aee8fb540f56b0
Philip Müller <philipmuller97@gmail.com>
2024-05-04 23:21:34 +0200

    Complete 3.1: Phonebook backend step 1 and 3.2: Phonebook backend step 2

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log('Hello, world!');
+const express = require('express')
+const app = express()
+
+let contacts = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (request, response) => {
+    const firstLineContent = `Phonebook has info for ${contacts.length} people`
+    const secondLineContent = new Date()
+    response.send(`<p>${firstLineContent}<br/><br/>${secondLineContent}</p>`)
+  })
+
+app.get('/api/persons', (request, response) => {
+  response.json(contacts)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
