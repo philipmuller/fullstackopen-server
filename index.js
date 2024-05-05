efdb4ec303cbb3c032b131d9df897422c6a371ff
Philip Müller <philipmuller97@gmail.com>
2024-05-05 04:14:36 +0200

    Finish exercises 3.15-3.18

--- index.js
+++ index.js
@@ -30,51 +30,18 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan(customFormatter))
 
-let contacts = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/', (request, response) => {
-  response.send('<h1>If this gets shown, something went wrong!</h1>')
+  response.send('<h1>If this gets shown, something went wrong! Failed to fetch react frontend</h1>')
 })
 
 app.get('/info', (request, response) => {
-    const firstLineContent = `Phonebook has info for ${contacts.length} people`
-    const secondLineContent = new Date()
-    response.send(`<p>${firstLineContent}<br/><br/>${secondLineContent}</p>`)
+    Person.find({})
+    .then(result => {
+        const firstLineContent = `Phonebook has info for ${result.length} people`
+        const secondLineContent = new Date()
+        response.send(`<p>${firstLineContent}<br/><br/>${secondLineContent}</p>`)
+    })
 })
-
-function generateId() {
-    const randomNum = Math.random() * 1000000
-    const randomInt = Math.floor(randomNum)
-
-    const finalId = randomInt
-
-    if (contacts.find(contact => contact.id === finalId)) {
-        return generateId() //calling itself recursively until a unique id is generated, a bit dangerous but should be fine
-    }
-
-    return finalId
-}
 
 app.post('/api/persons', (request, response) => {
     const contact = request.body
@@ -83,12 +50,6 @@ app.post('/api/persons', (request, response) => {
     if (!contact.name || !contact.number || contact.name === '' || contact.number === '') {
         return response.status(400).json({ 
             error: 'Name or number missing' 
-        })
-    }
-
-    if (contacts.find(cntct => cntct.name === contact.name)) {
-        return response.status(400).json({ 
-            error: 'Name already exists in the phonebook' 
         })
     }
 
@@ -118,37 +79,51 @@ app.get('/api/persons/:id', (request, response) => {
             response.status(404).end()
         }
     })
-    .catch(error => {
-        response.status(404).end()
-    })
-
-    // const id = Number(request.params.id)
-    // const contact = contacts.find(cntct => cntct.id === id)
-  
-    // if (contact) {
-    //     response.json(contact)
-    // } else {
-    //     response.status(404).end()
-    // }
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const filteredContacts = contacts.filter(contact => contact.id !== id)
+    Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+        if (result) {
+            response.status(204).end()
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
+})
 
-    if (filteredContacts.length === contacts.length) {
-        response.status(404).end()
-    } else {
-        contacts = filteredContacts
-        response.status(204).end()
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+  
+    const person = {
+      name: body.name,
+      number: body.number,
     }
+  
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedContact => {
+        response.json(updatedContact)
+      })
+      .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'Unknown endpoint' })
 }
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
   
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+
+    next(error)
+}
+
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
